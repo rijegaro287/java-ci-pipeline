003181cb86c3e77275fe28afff2f3a812f814118
rijegaro287 <gatgens48@gmail.com>
2021-03-26 14:03:18 -0600

    [update] cambios a build.xml

--- src/CalculadoraTest.java
+++ src/CalculadoraTest.java
@@ -1,35 +1,34 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-class CalculadoraTest {
-
+public class CalculadoraTest {
     @Test
-    void sumar() {
-        assertEquals(127, Calculadora.sumar(5, 122));
-        assertEquals(14109, Calculadora.sumar(8431, 5678));
+    public void sumar() {
+        assertEquals(127, Calculadora.sumar(5, 122), 0);
+        assertEquals(14109, Calculadora.sumar(8431, 5678),0);
     }
 
     @Test
-    void restar() {
-        assertEquals(-117, Calculadora.restar(5, 122));
-        assertEquals(2753, Calculadora.restar(8431, 5678));
+    public void restar() {
+        assertEquals(-117, Calculadora.restar(5, 122),0);
+        assertEquals(2753, Calculadora.restar(8431, 5678),0);
     }
 
     @Test
-    void multiplicar() {
-        assertEquals(610, Calculadora.multiplicar(5, 122));
-        assertEquals(-101172, Calculadora.multiplicar(8431, -12));
+    public void multiplicar() {
+        assertEquals(610, Calculadora.multiplicar(5, 122),0);
+        assertEquals(-101172, Calculadora.multiplicar(8431, -12),0);
     }
 
     @Test
-    void dividir() {
-        assertEquals(-1.25, Calculadora.dividir(-5, 4));
-        assertEquals(1053.75, Calculadora.dividir(8430, 8));
+    public void dividir() {
+        assertEquals(-1.25, Calculadora.dividir(-5, 4),0);
+        assertEquals(1053.75, Calculadora.dividir(8430, 8),0);
     }
 
     @Test
-    void potencias() {
-        assertEquals(625, Calculadora.potencias(-5, 4));
-        assertEquals(2097152, Calculadora.potencias(8, 7));
+    public void potencias() {
+        assertEquals(625, Calculadora.potencias(-5, 4),0);
+        assertEquals(2097152, Calculadora.potencias(8, 7),0);
     }
 }
